90c81920a73199547f1339cd78ac2f830415271f
mbrown <tugtuj@gmail.com>
2021-04-14 20:41:12 -0400

    Minor updates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 
 ReactDOM.render(
   <React.StrictMode>

fb7011009b4f86246b44f9f4a49530d90e71eed1
mbrown <tugtuj@gmail.com>
2021-04-14 20:40:39 -0400

    Home component

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Nav from './Nav';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Mocha Brown</h1>
+            <h3> Full-Stack Web Developer </h3>
+
+            <p> 
+            Javascript 
+            Html
+            CSS
+            Python
+            React
+            Node.js
+            Express.js
+            PostgreSQL
+            Sequelize
+            ORM
+            MongoDB
+            Mongoose
+            RESTful
+            Routing
+            JSON
+            APIs
+            </p>
+
+        </div>
+    );
+}
+
+export default Home;

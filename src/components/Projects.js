8670928253b6ac858633475d48629bbda5601ed4
BrownMY <tugtuj@gmail.com>
2021-04-23 22:57:09 -0400

    setup

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Projects = () => {
+return (
+    <div>
+        <div class="project" id="project1">
+            <img class="img1" src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene1_etkknp.png"/>
+            <img class="img2" src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene2_smpb7i.png"/>
+
+        </div>
+    </div>
+)
+}

d701ec70ba1ef7e529172dd601d7ed8d6de13a5b
BrownMY <tugtuj@gmail.com>
2021-04-26 20:56:24 -0400

    Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,31 +17,31 @@ const Projects = () => {
         <div class="project" id="project2">
           <img
             class="img1"
-            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene1_etkknp.png"
+            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526303/farmony1_ticyyk.png"
           />
           <img
             class="img2"
-            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene2_smpb7i.png"
+            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526304/farmony2_rchv8z.png"
           />
         </div>
         <div class="project" id="project3">
           <img
             class="img1"
-            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene1_etkknp.png"
+            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526308/muse1_t8d5fc.png"
           />
           <img
             class="img2"
-            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene2_smpb7i.png"
+            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526301/muse2_t9yd64.png"
           />
         </div>
         <div class="project" id="project4">
           <img
             class="img1"
-            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene1_etkknp.png"
+            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526294/door-defense1_necscw.png"
           />
           <img
             class="img2"
-            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526306/selene2_smpb7i.png"
+            src="https://res.cloudinary.com/doihe1pi6/image/upload/v1618526293/door-defense2_brtk1d.png"
           />
         </div>
       </div>

e7788c4921abafcb76fa4c2309dea4712fdd69de
BrownMY <tugtuj@gmail.com>
2021-04-28 14:12:30 -0400

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
+        <div class="contact-container">
             <h4><a href="https://github.com/BrownMY">Github</a></h4>
             <h4><a href="https://www.linkedin.com/in/mocha-brown/">LinkedIn</a></h4>
             <h4>Email: Brown.MochaY@gmail.com</h4>

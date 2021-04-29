cc3500352ec17c10de425da69cec58f6747f7207
BrownMY <tugtuj@gmail.com>
2021-04-29 16:13:50 -0400

    Contact- resume, email added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,63 @@
-import React from 'react'
-import GitHubIcon from './icons/GithubIcon';
-import LinkedinIcon from './icons/LinkedInIcon';
-import EmailIcon from './icons/EmailIcon';
-
+import React from "react";
+import GitHubIcon from "./icons/GithubIcon";
+import LinkedinIcon from "./icons/LinkedInIcon";
+import EmailIcon from "./icons/EmailIcon";
+import ResumeIcon from "./icons/ResumeIcon";
 
 const Contact = () => {
-    return (
-        <div>
-            <h2 class="h3-contact">Links & Contact:</h2>
-         <div class="contact-container">
-             <div class="link-container">
-                 <GitHubIcon /><h4><a class="a-contact" href="https://github.com/BrownMY" target="_blank" rel="noreferrer">Github</a></h4>
-             </div>
-             <div class="link-container">
-                 <LinkedinIcon /><h4><a class="a-contact" href="https://www.linkedin.com/in/mocha-brown/" target="_blank" rel="noreferrer">LinkedIn</a></h4>
-             </div>
-             <div class="link-container">
-                 <EmailIcon /><h4><a class="a-contact" href="Brown.MochaY@gmail.com" target="_blank" rel="noreferrer">Email</a></h4>
-             </div>
-    
-        </div>   
+  return (
+    <div>
+      <h2 class="h3-contact">Links & Contact:</h2>
+      <div class="contact-container">
+        <div class="contact-top-row">
+
+        <div class="link-container">
+          <GitHubIcon />
+          <h4>
+            <a
+              class="a-contact"
+              href="https://github.com/BrownMY"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </h4>
         </div>
-        
-    )
-}
+        <div class="link-container">
+          <LinkedinIcon />
+          <h4>
+            <a
+              class="a-contact"
+              href="https://www.linkedin.com/in/mocha-brown/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </h4>
+        </div>
+        <div class="link-container">
+          <ResumeIcon />
+          <h4>
+            <a
+              class="a-contact"
+              href="https://www.canva.com/design/DAEbq9eqvNM/864q0OVaKqghFJ5f9Kp4MA/view?utm_content=DAEbq9eqvNM&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu" target="_blank" rel="noreferrer">
+              Resume
+            </a>
+          </h4>
+        </div>
+        </div>
+        <br />
+        <div class="contact-bottom-row">
+          <div class="link-container">
+            <EmailIcon />
+            <h4>Brown.MochaY@gmail.com</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;

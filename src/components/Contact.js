2fbbd382eb32c573ea93ae355421727a9a1a62e8
mbrown <tugtuj@gmail.com>
2025-02-05 22:24:06 -0500

    updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,43 +10,45 @@ const Contact = () => {
       <h2 class="h3-contact">Links & Contact:</h2>
       <div class="contact-container">
         <div class="contact-top-row">
-
-        <div class="link-container">
-          <GitHubIcon />
-          <h4>
-            <a
-              class="a-contact"
-              href="https://github.com/BrownMY"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-          </h4>
-        </div>
-        <div class="link-container">
-          <LinkedinIcon />
-          <h4>
-            <a
-              class="a-contact"
-              href="https://www.linkedin.com/in/mocha-brown/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              LinkedIn
-            </a>
-          </h4>
-        </div>
-        <div class="link-container">
-          <ResumeIcon />
-          <h4>
-            <a
-              class="a-contact"
-              href="https://www.canva.com/design/DAEbq9eqvNM/864q0OVaKqghFJ5f9Kp4MA/view?utm_content=DAEbq9eqvNM&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu" target="_blank" rel="noreferrer">
-              Resume
-            </a>
-          </h4>
-        </div>
+          <div class="link-container">
+            <GitHubIcon />
+            <h4>
+              <a
+                class="a-contact"
+                href="https://github.com/BrownMY"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </h4>
+          </div>
+          <div class="link-container">
+            <LinkedinIcon />
+            <h4>
+              <a
+                class="a-contact"
+                href="https://www.linkedin.com/in/mocha-brown/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </h4>
+          </div>
+          <div class="link-container">
+            <ResumeIcon />
+            <h4>
+              <a
+                class="a-contact"
+                href="https://www.canva.com/design/DAF964Wjcbc/H85DSPNJDKUQ2NpoUO-9aw/view?utm_content=DAF964Wjcbc&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hcbf9bcb071"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Resume
+              </a>
+            </h4>
+          </div>
         </div>
         <br />
         <div class="contact-bottom-row">
